6f89dd928daa9e02ce66efd69e671f06e2962a41
najiya817 <najiyanaji817@gmail.com>
2023-02-15 14:29:38 +0530

    15feb

--- openblog/account/urls.py
+++ openblog/account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
-
+        path('reg/',RegView.as_view(),name="reg"),
+        
 ]
--- openblog/account/views.py
+++ openblog/account/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import RegForm
 
 # Create your views here.
 
 class HomeView(View):
     def get(self,request,*args,**kwargs):
         return render(request,"main_home.html")
+
+class RegView(View):
+    def get(self,request,*args,**kwargs):
+        f=RegForm
+        return render(request,"reg.html",{"form":f})
+    
+    def post(self,request,*args,**kwargs):
+        form_data=RegForm(data=request.POST)
+        if form_data=
